c22250d2cb9b6207d4d34fc3d1dcdf9d26e61ad4
the-darknes <magist.dev@hotmail.com>
2025-05-24 12:08:30 +0300

    fix(cors): configure CORS to allow requests from localhost with credentials

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors();
+	app.enableCors({
+		origin: 'http://localhost:8080',
+		credentials: true,
+	});
 	app.setGlobalPrefix('/api/v1');
 	await app.listen(process.env.PORT ?? 3000);
 }
